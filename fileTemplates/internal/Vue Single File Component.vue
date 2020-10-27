02f91581116d56a8a88d82f5266885a7ce814ecd
Burt <2902978956@qq.com>
2020-10-27 21:45:54 +0800

    WS-2020.2 <Burt@DESKTOP-IC84I75 Create baseRefactoring.xml, markdown.xml, Vue Single File Component.vue, ide.general.xml, git.xml, project.default.xml, Default.xml, applicationLibraries.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, colors.scheme.xml, gutter.xml, Windows copy.xml, emmet.xml, laf.xml, _@user_Monokai Pro.icls, wsl.distributions.xml, material_theme.xml, dbnavigator.xml, vcs.xml, editor.xml, debugger.xml, a-file-icons.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,14 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+export default class ${COMPONENT_NAME} extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
